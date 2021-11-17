7e1ce96563405dac2d109e76011ef3879f427d07
Alejandro Phillips <aphillips@uc.cl>
2021-11-17 12:55:30 -0300

    add get and create restaurant endpoints

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import List
+
+import crud, models, schema
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'Hello World'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/restaurants', response_model=List[schema.Restaurant])
+def read_restaurants(db: Session=Depends(get_db)):
+    restaurants = crud.get_restaurants(db)
+    return restaurants
+
+@app.post('/restaurants', response_model=schema.Restaurant)
+def create_restaurant(restaurant: schema.RestaurantCreate, db: Session=Depends(get_db)):
+    return crud.create_restaurant(db=db, restaurant=restaurant)
+    
