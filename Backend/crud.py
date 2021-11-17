34b828d8524931b03b42b110a5c69a147e10a8f6
Alejandro Phillips <aphillips@uc.cl>
2021-11-17 17:49:19 -0300

    add update restaurant endpoint

--- Backend/crud.py
+++ Backend/crud.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
 import models, schema
 
+def get_restaurant_by_id(db: Session, restaurant_id: int):
+    return db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
+
 def get_restaurants(db: Session):
     return db.query(models.Restaurant).all()
 
@@ -10,6 +13,11 @@ def create_restaurant(db: Session, restaurant: schema.RestaurantCreate):
     db.commit()
     db.refresh(db_restaurant)
     return db_restaurant
+
+def update_restaurant(db: Session, restaurant: schema.Restaurant):
+    db.query(models.Restaurant).filter(models.Restaurant.id == restaurant.id).update(restaurant.dict())
+    db.commit()
+    return restaurant
 
 def delete_restaurant(db: Session, restaurant_id: int):
     restaurant = db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -24,6 +24,13 @@ def read_restaurants(db: Session=Depends(get_db)):
 @app.post('/restaurants', response_model=schema.Restaurant)
 def create_restaurant(restaurant: schema.RestaurantCreate, db: Session=Depends(get_db)):
     return crud.create_restaurant(db=db, restaurant=restaurant)
+
+@app.post('/restaurants/update', response_model=schema.Restaurant)
+def update_restaurant(restaurant: schema.Restaurant, db: Session=Depends(get_db)):
+    if crud.get_restaurant_by_id(db=db, restaurant_id=restaurant.id):
+        return crud.update_restaurant(db=db, restaurant=restaurant)
+    else:
+        raise HTTPException(status_code=400, detail='Restaurant no existe')
 
 @app.delete('/restaurants/{restaurant_id}', response_model=schema.Restaurant)
 def delete_restaurant(restaurant_id: int, db: Session=Depends(get_db)):
